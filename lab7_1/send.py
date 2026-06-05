fd4c5790a0c6d592151b69e864a9587bdca03826
Art Mac <anandezgarcia@gmail.com>
2026-06-04 17:38:11 -0700

    fix send

---
+++ lab7_1/send.py
@@ -0,0 +1,58 @@
+from gpiozero import LED
+from time import sleep
+import sys
+
+led = LED(18)
+
+MORSE = {
+    'A': '.-',    'B': '-...',  'C': '-.-.',
+    'D': '-..',   'E': '.',     'F': '..-.',
+    'G': '--.',   'H': '....',  'I': '..',
+    'J': '.---',  'K': '-.-',   'L': '.-..',
+    'M': '--',    'N': '-.',    'O': '---',
+    'P': '.--.',  'Q': '--.-',  'R': '.-.',
+    'S': '...',   'T': '-',
+    'U': '..-',   'V': '...-',  'W': '.--',
+    'X': '-..-',  'Y': '-.--',  'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---',
+    '3': '...--', '4': '....-', '5': '.....',
+    '6': '-....', '7': '--...', '8': '---..',
+    '9': '----.'
+}
+
+DOT = 0.1
+DASH = 0.3
+
+def send_symbol(symbol):
+    led.on()
+
+    if symbol == '.':
+        sleep(DOT)
+    else:
+        sleep(DASH)
+
+    led.off()
+    sleep(DOT)
+
+def send_message(msg):
+    for ch in msg.upper():
+        if ch == ' ':
+            sleep(0.7)
+            continue
+
+        if ch in MORSE:
+            for symbol in MORSE[ch]:
+                send_symbol(symbol)
+
+            sleep(0.3)
+
+if len(sys.argv) != 3:
+    print("Usage: python3 send.py <count> \"message\"")
+    sys.exit(1)
+
+count = int(sys.argv[1])
+message = sys.argv[2]
+
+for _ in range(count):
+    send_message(message)
+    sleep(1)}
