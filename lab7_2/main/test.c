df99382eb2bc007690f65966fe9d407239b0d106
Art Mac <anandezgarcia@gmail.com>
2026-06-04 18:21:52 -0700

    update for test

--- lab7_2/main/test.c
+++ lab7_2/main/test.c
@@ -1,22 +1,49 @@
+```c
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 
-ESP_ERROR_CHECK(
-    adc_oneshot_config_channel(
-        adc_handle,
-        ADC_CHANNEL_1,
-        &config));
+#include "esp_adc/adc_oneshot.h"
+#include "esp_log.h"
 
-while (1)
+static const char *TAG = "MORSE_RX";
+
+void app_main(void)
 {
-    int adc_raw = 0;
+    adc_oneshot_unit_handle_t adc_handle;
+
+    adc_oneshot_unit_init_cfg_t init_config = {
+        .unit_id = ADC_UNIT_1,
+    };
 
     ESP_ERROR_CHECK(
-        adc_oneshot_read(
+        adc_oneshot_new_unit(&init_config, &adc_handle));
+
+    adc_oneshot_chan_cfg_t config = {
+        .bitwidth = ADC_BITWIDTH_DEFAULT,
+        .atten = ADC_ATTEN_DB_12,
+    };
+
+    // CHANGE THIS IF YOUR SENSOR IS ON A DIFFERENT PIN
+    ESP_ERROR_CHECK(
+        adc_oneshot_config_channel(
             adc_handle,
             ADC_CHANNEL_1,
-            &adc_raw));
+            &config));
 
-    printf("ADC: %d\n", adc_raw);
+    while (1)
+    {
+        int adc_raw = 0;
 
-    vTaskDelay(pdMS_TO_TICKS(100));
+        ESP_ERROR_CHECK(
+            adc_oneshot_read(
+                adc_handle,
+                ADC_CHANNEL_1,
+                &adc_raw));
+
+        printf("ADC: %d\n", adc_raw);
+
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
-
+```
